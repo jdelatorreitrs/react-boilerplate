c1ae4acf5de263adf94c73c5175be82eba1910fd
Zuri Pabon <zpabon@itrsgroup.com>
2017-07-05 14:01:16 +0200

    removed button primary

---
+++ app/containers/Header/themes.js
@@ -0,0 +1,14 @@
+
+
+export const light = {
+  mainBgColor: 'rgba(0, 0, 0, 0.1)',
+  footerBgColor: 'rgba(0, 0, 0, 0.2)',
+  fontColor: "rgba(0, 0, 0, 0.5)"
+};
+
+
+export const dark = {
+  mainBgColor: '#262728',
+  footerBgColor: '#3a3b3d',
+  fontColor: "#c6c6c6"
+};
